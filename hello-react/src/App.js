8c4dc1def92e24c55d2fd39845b4ada05e99c682
jang95 <ruin950521@naver.com>
2022-06-07 22:49:05 +0900

    Study Day 4 chapter2

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { Fragment } from 'react';
+// import { Fragment } from 'react';
 
 /**
  * 이 코드는 App이라는 컴포넌트를 만들어 줍니다.
@@ -10,8 +10,15 @@ import { Fragment } from 'react';
 
 function App() {
   const name = '리액트';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    paddgin: 16
+  };
   return (
-    <div className="App">
+    <div className="App" style={style}>{name}
       {/* 리액트 컴포넌트에서 요소 여러 개를 왜 하나의 요소로 꼭 감싸 주어야 할까?
         -> Virtual DOM에서 컴포넌트 변화를 감지해 낼 때 효율적으로 비교할 수 있도록 컴포넌트 내부는 하나의 DOM 트리 구조로 이루어져야 한다는 규칙이 있기 때문
          
@@ -19,8 +26,6 @@ function App() {
          => <Fragment></Fragment> or <></> */}
       <div>
         {/* JSX 안에서 자바스크립트 표현식을 작성하려면 JSX 내부에서 코드를 {}로 감싸면 됩니다. */}
-        <h1>{name} 안녕!</h1> 
-        <h2>잘 작동하니?</h2>
       </div>
     </div>
   );
