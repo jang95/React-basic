ad906c9b9b308cad54bca30a27810bc4238a94f5
jang95 <ruin950521@naver.com>
2022-06-08 23:32:27 +0900

    Study Day 5 chapter3 (component)

---
+++ hello-react/src/App_basic.js
@@ -0,0 +1,34 @@
+// import logo from './logo.svg';
+import './App.css';
+// import { Fragment } from 'react';
+
+/**
+ * 이 코드는 App이라는 컴포넌트를 만들어 줍니다.
+ * function 키워드를 사용하여 컴포넌트 제작 => 이러한 컴포넌트를 함수형 컴포넌트라고 부릅니다.
+ * 프로젝트에서 컴포넌트를 렌더링하면(표출) 함수에서 반환하고 있는 내용을 나타냅니다.
+ */
+
+function App() {
+  const name = '리액트';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    paddgin: 16
+  };
+  return (
+    <div className="App" style={style}>{name}
+      {/* 리액트 컴포넌트에서 요소 여러 개를 왜 하나의 요소로 꼭 감싸 주어야 할까?
+        -> Virtual DOM에서 컴포넌트 변화를 감지해 낼 때 효율적으로 비교할 수 있도록 컴포넌트 내부는 하나의 DOM 트리 구조로 이루어져야 한다는 규칙이 있기 때문
+         
+        꼭 아래 코드처럼 div 요소를 사용하는 것이 아니라 리액트 v16이상 부터 도입된 Fragment라는 기능을 사용할 수 있음
+         => <Fragment></Fragment> or <></> */}
+      <div>
+        {/* JSX 안에서 자바스크립트 표현식을 작성하려면 JSX 내부에서 코드를 {}로 감싸면 됩니다. */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ hello-react/src/Mycomponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Mycomponent = () => {
+  return <div>나의 새롭고 멋진 컴포넌트</div>
+};
+
+// 다른 파일에서 이 파일을 import할 때, 위에서 선언한 MyComponent 클래스를 불러오도록 설정
+export default Mycomponent;
