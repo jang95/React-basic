cf61717ba8595d9a9385324a3e963557f59a4886
jang95 <ruin950521@naver.com>
2022-06-10 21:52:03 +0900

    Study Day 6 (prop2)

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Mycomponent from './Mycomponent';
 
 const App = () => {
-  return <Mycomponent name="장지호">리액트</Mycomponent>
+  return (
+    <Mycomponent name="장지호" favoriteNumber = {19970609}>리액트</Mycomponent>
+  )
 };
 
 export default App;
--- hello-react/src/Mycomponent.js
+++ hello-react/src/Mycomponent.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const Mycomponent = props => {
+// 함수의 파라미터가 객체라면 그 값을 바로 비구조화해서 사용 가능
+const Mycomponent = ({ name, favoriteNumber, children}) => {
+  // 비구조화 할당(구조 분해 문법)
+  // const { name, children} = props;
   return (
     <div>
-      안녕하세요, 제 이름은 {props.name} 입니다. <br/>
+      안녕하세요, 제 이름은 {name} 입니다. <br/>
       {/* 컴포넌트 태그 사이의 내용을 보여주는 props => children */}
-      children 값은 {props.children} 입니다.
+      children 값은 {children} 입니다.
+      <br />
+      제가 좋아하는 숫자는 {favoriteNumber} 입니다.
     </div>
     )
 };
@@ -15,6 +21,11 @@ const Mycomponent = props => {
  */
 Mycomponent.defaultProps = {
   name: '기본 이름'
+};
+
+Mycomponent.prototype = {
+  name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired
 };
 
 // 다른 파일에서 이 파일을 import할 때, 위에서 선언한 MyComponent 클래스를 불러오도록 설정
