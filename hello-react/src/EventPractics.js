36bdbff54c14a66a06f80bbbe57fac0643b5c28f
jang95 <ruin950521@naver.com>
2022-06-16 18:43:30 +0900

    Study Day 10 (Chapter4 bind(this) )

--- hello-react/src/EventPractics.js
+++ hello-react/src/EventPractics.js
@@ -6,6 +6,25 @@ class EventPractice extends Component {
     message: ''
   }
 
+  constructor(props) {
+    super(props);
+    this.handlerChange = this.handlerChange.bind(this);
+    this.handlerClick = this.handlerClick.bind(this);
+  }
+
+  handlerChange(e) {
+    this.setState({
+      message: e.target.value
+    });
+  }
+
+  handlerClick() {
+    alert(this.state.message);
+    this.setState({
+      message: ''
+    });
+  }
+
   render() {
     return (
       <div>
@@ -15,29 +34,31 @@ class EventPractice extends Component {
           name = "message"
           placeholder='아무거나 입력해 보세요'
           value={this.state.message}
-          onChange={
-            /**
-             * @param {*} e SyntheticEvent로 웹 브라우저의 네이티브 이벤트를 감싸는 객체
-             *              네이티브 이벤트와 인터페이스가 같으므로 순수 자바스크립트에서
-             *              HTML 이벤트를 다룰 때랑 똑같이 사용
-             * 
-             * SyntheticEvent는 네이티브 이벤트와 달리 끝나면 바로 초기화
-             */
-            (e) => {
-              this.setState({
-                message: e.target.value
-              });
-              console.log(e.target.value);
-            }
-          }
+          // onChange={
+          //   /**
+          //    * @param {*} e SyntheticEvent로 웹 브라우저의 네이티브 이벤트를 감싸는 객체
+          //    *              네이티브 이벤트와 인터페이스가 같으므로 순수 자바스크립트에서
+          //    *              HTML 이벤트를 다룰 때랑 똑같이 사용
+          //    * 
+          //    * SyntheticEvent는 네이티브 이벤트와 달리 끝나면 바로 초기화
+          //    */
+          //   (e) => {
+          //     this.setState({
+          //       message: e.target.value
+          //     });
+          //     console.log(e.target.value);
+          //   }
+          // }
+          onChange={this.handlerClick}
         />
-        <button onClick={
+        {/* <button onClick={
           () => {
             alert(this.state.message);
             this.setState({
               message: ''
             });
-        }}>확인</button>
+        }}>확인</button> */}
+        <button onClick={this.handlerClick}>확인</button>
       </div>
     );
   }
