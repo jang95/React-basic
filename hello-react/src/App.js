9836b9138b9f972c3c0b42550d87b7040ae3d1f1
jang95 <ruin950521@naver.com>
2022-06-26 16:23:02 +0900

    Study Day 12 (Chapter 5 ref basic)

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+// import React from "react";
+import React, {Component} from 'react';
 // import Mycomponent from './Mycomponent';
 // import Counter from "./Counter";
 // import Say from "./Say";
 // import EventPractice from "./EventPractics";
-import EventPracticeFun from "./EventPractics_Fun";
+// import EventPracticeFun from "./EventPractics_Fun";
+import ValidationSample from './ValidationSample';
 
-const App = () => {
-  return <EventPracticeFun />;
-};
+// const App = () => {
+//   return <EventPracticeFun />;
+// };
+
+class App extends Component {
+  render() {
+    return (
+      <ValidationSample></ValidationSample>
+    )
+  }
+}
 
 export default App;
