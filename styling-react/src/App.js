34970701114bc6b3a23efea4d6bd9a2f2c71e6ad
jang95 <ruin950521@naver.com>
2022-07-23 09:55:27 +0900

    Study Day 22 css

---
+++ styling-react/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SassComponent from './SassComponent';
+// import logo from './logo.svg';
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <SassComponent />
+    );
+  }
+}
+
+export default App;
