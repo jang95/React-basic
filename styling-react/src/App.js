8ba813e40f4c42467147ade1454ed98629ea997b
jang95 <ruin950521@naver.com>
2022-07-24 11:43:20 +0900

    Study Day 23 (CSS Module, classnames, styled-components)

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import SassComponent from './SassComponent';
+// import SassComponent from './SassComponent';
 // import logo from './logo.svg';
 // import './App.css';
+// import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
 
 class App extends Component {
   render() {
     return (
-      <SassComponent />
+      // <CSSModule />
+      <StyledComponent />
     );
   }
 }
