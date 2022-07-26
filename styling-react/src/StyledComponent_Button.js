346e7468b4ef0a4f322022b9bce2c8433c9b2069
jang95 <ruin950521@naver.com>
2022-07-26 21:39:49 +0900

    Study Day 24(CSS final)

--- styling-react/src/StyledComponent_Button.js
+++ styling-react/src/StyledComponent_Button.js
@@ -1,27 +1,10 @@
 import React from "react";
 import styled, { css } from "styled-components";
-
-const sizes = {
-  desktop: 1024,
-  tablet: 768
-};
-
-/* 위에 있는 size 객체에 따라 자동으로 media 쿼리 함수를 만들어 줌
-   참고: https://www.styled-components.com/docs/advanced#media-templates
+/* 단순 변수의 형태가 아니라 여러 줄의 스타일 구문을 조건부로 설정해야 하는 경우에는
+   css를 불러와야 합니다.
 */
-const media = Object.keys(sizes).reduce((acc, label) => {
-  acc[label] = (...args) => css`
-  @media (max-width: ${sizes[label] / 16}em) {
-    ${css(...args)};
-  }
-  `;
-  console.log('acc', acc);
-  console.log('label', label);
-  return acc;
-}, {});
 
 const Box = styled.div`
-  background: ${(props) => props.color || "blue"};
   padding: 1rem;
   display: flex;
   /* 기본적으로 가로 크기 1024px에 가운데 정렬을 하고
@@ -30,8 +13,12 @@ const Box = styled.div`
   */
   width: 1024px;
   margin: 0 auto;
-  ${media.desktop`width: 768px;`}
-  ${media.tablet`width: 100%;`};
+  @media (max-width: 1020px) {
+    width: 768px;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 const Button = styled.button`
