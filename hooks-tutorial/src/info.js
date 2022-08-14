c545f8043eb0b76e0efbd5b0432a17ad52689b46
jang95 <ruin950521@naver.com>
2022-08-14 18:38:11 +0900

    Study Day34 커스텀 Hook(usePromise) 1. Promise의 대기/완료 결과/실패 경과에 대한 상태관리 2. 의존 배열 deps -> useEffect의 의존 배열로 설정

--- hooks-tutorial/src/info.js
+++ hooks-tutorial/src/info.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
   /**
    * useEffect 리액트 컴포넌트가 렌더링될 때마다 특정 작업을 수행하도록 설정할 수 있는 Hook
    * 클래스형 컴포넌트의 componentDidMount와 componentDidUpdate를 합친 형태로 생각해도 무방
-   * 
+   *
    * useEffect에서 설정한 함수를 컴포넌트가 화면에 맨 처음 렌더링될 때만 실행하고,
-   * 업데이트 될 때는 실행하지 않으려면 함수의 두 번째 파마리터로 비어 있는 배열을 넣어 주면 됩니다. 
+   * 업데이트 될 때, 실행하지 않으려면 함수의 두 번째 파마리터로 비어 있는 배열을 넣어 주면 됩니다.
    */
 
   // useEffect(() => {
@@ -36,19 +36,19 @@ const Info = () => {
    */
 
   useEffect(() => {
-    console.log('effect');
+    console.log("effect");
     console.log(name);
     return () => {
-      console.log('cleanup');
+      console.log("cleanup");
       console.log(name);
-  }
+    };
   }, [name]);
 
-  const onChangeName = e => {
+  const onChangeName = (e) => {
     setName(e.target.value);
   };
 
-  const onChangeNickname = e => {
+  const onChangeNickname = (e) => {
     setNickname(e.target.value);
   };
 
--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import NewItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 // API를 요청하고 뉴스 데이터가 들어 있는 배열을 컴포넌트 배열로 변환하여 렌더링해 주는 컴포넌트
 const NewsitemBlock = styled.div`
@@ -24,39 +26,78 @@ const NewsitemBlock = styled.div`
 //   urlToImage: 'https://via.placeholder.com/160',
 // };
 
-const NewsList = ({ categoryObject }) => {
-  const [article, setArticle] = useState(null);
-  const [loading, setLoading] = useState(false);
-  console.log('NewList:Category', categoryObject);
+// const NewsList = ({ categoryObject }) => {
+//   const [article, setArticle] = useState(null);
+//   const [loading, setLoading] = useState(false);
+//   console.log('NewList:Category', categoryObject);
 
-  useEffect(() => {
-    // async를 사용하는 함수 따로 선언
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query =
-          categoryObject === 'all' ? '' : `&category=${categoryObject}`;
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=28789fcecf524f2e8e75b1513d6479b1`,
-        );
-        setArticle(response.data.articles);
-        console.log('query', query);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+//   useEffect(() => {
+//     // async를 사용하는 함수 따로 선언
+//     const fetchData = async () => {
+//       setLoading(true);
+//       try {
+//         const query =
+//           categoryObject === 'all' ? '' : `&category=${categoryObject}`;
+//         const response = await axios.get(
+//           `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=28789fcecf524f2e8e75b1513d6479b1`,
+//         );
+//         setArticle(response.data.articles);
+//         console.log('query', query);
+//       } catch (e) {
+//         console.log(e);
+//       }
+//       setLoading(false);
+//     };
+//     fetchData();
+//   }, [categoryObject]);
+
+//   // 대기 중일 때
+//   if (loading) {
+//     return <NewsitemBlock>대기 중...</NewsitemBlock>;
+//   }
+//   // 아직 articles 값이 설정되지 않았을 때
+//   if (!article) {
+//     return null;
+//   }
+
+//   // article 값이 유효할 때
+//   return (
+//     <NewsitemBlock>
+//       {article.map((article) => (
+//         <NewItem key={article.url} article={article} />
+//       ))}
+//     </NewsitemBlock>
+//   );
+// };
+
+const NewsList = ({ categoryObject }) => {
+  const [loading, response, error] = usePromise(() => {
+    const query = categoryObject === 'all' ? '' : `&category=${categoryObject}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=28789fcecf524f2e8e75b1513d6479b1`,
+    );
   }, [categoryObject]);
 
   // 대기 중일 때
   if (loading) {
     return <NewsitemBlock>대기 중...</NewsitemBlock>;
   }
-  // 아직 articles 값이 설정되지 않았을 때
-  if (!article) {
+
+  // 아직 respons 값이 설정되지 않았을 때
+  if (!response) {
     return null;
   }
+
+  // 에러가 발생했을 때
+  if (error) {
+    return <NewsitemBlock>에러 발생!</NewsitemBlock>;
+  }
+
+  // response 값이 유효할 때
+  const article = response.data.articles;
+  // const {articles} = response.data;
+
+  console.log('response', response);
 
   // article 값이 유효할 때
   return (
