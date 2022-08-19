71fc57d76cdb7d5d04a63d3c315e31ca973e60ba
jang95 <ruin950521@naver.com>
2022-08-19 21:35:00 +0900

    Study Day 37 redux basic createStore => configureStore 변경 필요

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -1,1 +1,81 @@
-console.log("hello parcel");
+import { createStore } from "redux";
+// import { configureStore } from "redux";
+
+const divToggle = document.querySelector("toggle");
+const counter = document.querySelector("h1");
+const btnIncrease = document.querySelector("#increase");
+const btnDecrease = document.querySelector("#decrease");
+
+// 프로젝트의 상태에 변화를 일으키는 것을 액션이라고 함
+
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+// 액션 객체를 만드는 액션 생성 함수
+const toggleSwich = () => ({ type: TOGGLE_SWITCH });
+const increase = (difference) => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
+
+// 초기값 설정
+const initialState = {
+  toggle: false,
+  counter: 0,
+};
+
+// state가 undifined일 때는 initalState를 기본값으로 사용
+function reducer(state = initialState, action) {
+  // action.type에 따라 다른 작업을 처리함
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state, // 불변성 유지를 해 주어야 함
+        toggle: !state.toggle,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+}
+
+// 스토어 생성
+// const store = createStore(reducer);
+// const store = configureStore({
+//   reducer: {
+//     reducer,
+//   },
+// });
+
+const render = () => {
+  const state = store.getState(); // 현재 상태를 불러옴
+  // 토글 처리
+  if (state.toggle) {
+    divToggle.classList.add("action");
+  } else {
+    divToggle.classList.remove("action");
+  }
+  // 카운터 처리
+  counter.innerText = state.counter;
+};
+
+render();
+store.subscribe(render);
+
+divToggle.onclick = () => {
+  store.dispatch(toggleSwich());
+};
+btnIncrease.onclick = () => {
+  store.dispatch(increase(1));
+};
+btnDecrease.onclick = () => {
+  store.dispatch(decrease());
+};
