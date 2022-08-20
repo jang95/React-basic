54c8610e7745ab4828d733e867eef338464c1453
jang95 <ruin950521@naver.com>
2022-08-20 15:21:38 +0900

    Study Day 38 redux react에 적용하기 v 4.1.2 -> createStore 사용하기 위해 counter, todos modules

---
+++ react-redux-tutorial/src/modules/counter.js
@@ -0,0 +1,38 @@
+/**
+ * 액션 타입 정의
+ * 액션 타입 대문자 = '모듈 이름/액션 이름'
+ * -> 액션 이름이 충돌되는 것을 방지
+ */
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+/**
+ * 액션 생성 함수
+ */
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+/**
+ * 초기 상태 및 리듀서 함수 만들기
+ */
+
+const initalState = {
+  number: 0,
+};
+
+function counter(state = initalState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return {
+        number: state.number + 1,
+      };
+    case DECREASE:
+      return {
+        number: state.number - 1,
+      };
+    default:
+      return state;
+  }
+}
+
+export default counter;
