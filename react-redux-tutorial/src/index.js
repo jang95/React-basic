34a1fc966cd33305af99abe9280ab9513c27b4c4
jang95 <ruin950521@naver.com>
2022-08-21 12:30:45 +0900

    Study Day 37 Redux devtools 적용

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// ReactDOM은 최신 버전에서 사용하지 않음
+// import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import rootReducer from './modules';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools());
+
+const root = createRoot(document.getElementById('root'));
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root'),
+// );
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
