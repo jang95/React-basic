6cd338dba1a5d46e67b8d70884ea3e4ea27b4751
jang95 <ruin950521@naver.com>
2022-08-28 20:26:41 +0900

    Study Day 40 redux-action

--- react-redux-tutorial/src/modules/counter.js
+++ react-redux-tutorial/src/modules/counter.js
@@ -1,3 +1,4 @@
+import { createAction, handleActions } from 'redux-actions';
 /**
  * 액션 타입 정의
  * 액션 타입 대문자 = '모듈 이름/액션 이름'
@@ -9,8 +10,11 @@ const DECREASE = 'counter/DECREASE';
 /**
  * 액션 생성 함수
  */
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // export const actionIncrease = () => ({ type: INCREASE });
 // export const actionDecrease = () => ({ type: DECREASE });
@@ -23,19 +27,27 @@ const initalState = {
   number: 0,
 };
 
-function counter(state = initalState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initalState,
+);
+
+// function counter(state = initalState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1,
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// }
 
 export default counter;
