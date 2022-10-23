102c62141ba712ef252565a9291e012d7c090a4a
jang95 <ruin950521@naver.com>
2022-10-23 22:40:59 +0900

    use koa-router

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,6 +1,28 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.get('/', (ctx) => {
+  ctx.body = '홈';
+});
+
+router.get('/about:name?', (ctx) => {
+  const { name } = ctx.params;
+  // name의 존재 유무에 따라 다른 결과 출력
+  ctx.body = name ? `${name}의 소개` : '소개';
+});
+
+router.get('/:posts?', (ctx) => {
+  const { id } = ctx.query;
+  // name의 존재 유무에 따라 다른 결과 출력
+  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
+});
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 
 /**
  * app.use: 미들웨어 함수를 애플리케이션에 등록
@@ -9,28 +31,7 @@ const app = new Koa();
  * @param next 현재 처리 중인 미들웨어의 다음 미들웨어를 호출하는 함수
  *             next 함수를 호출하지 않으면, 그다음 미들웨어를 처리하지 않음
  */
-app.use(async (ctx, next) => {
-  console.log(ctx.url);
-  console.log(1);
-  if (ctx.query.authorized !== '1') {
-    ctx.status = 401; // Unauthorized
-    return;
-  }
-  // next().then(() => {
-  //   console.log('END');
-  // });
-  await next();
-  console.log('END');
-});
-
-app.use((ctx, next) => {
-  console.log(2);
-  next();
-});
-
-app.use((ctx) => {
-  ctx.body = 'hello world';
-});
+//
 
 // 서버 포트 4000으로 열고, 서버에 접속하면 'hello world'라는 텍스트를 반환하도록 설정
 app.listen(4000, () => {
