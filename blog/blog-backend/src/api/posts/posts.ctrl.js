dff1dbfaf8677d11fbbd92a1b61df060057ea11f
jang95 <ruin950521@naver.com>
2022-11-15 19:18:30 +0900

    ADD: list, read, remove, update

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -15,10 +15,52 @@ export const write = async (ctx) => {
   }
 };
 
-export const list = (ctx) => {};
+export const list = async (ctx) => {
+  try {
+    const posts = await Post.find().exec(); // find() 함수를 호출한 후에는 exec()를 붙여 주어야 서버에 쿼리 요청.
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
-export const read = (ctx) => {};
+export const read = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findById(id).exec();
+    if (!post) {
+      ctx.status = 404; // Not Found
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
-export const remove = (ctx) => {};
+export const remove = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
-export const update = (ctx) => {};
+export const update = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true, // 이 값을 설정하면 업데이트된 데이터를 반환
+      // false일 때는 업데이트되기 전의 데이터를 반환.
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
