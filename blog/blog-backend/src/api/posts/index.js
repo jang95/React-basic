5fa4b88c01c15b4ef4a07790bc48546a20e2126f
jang95 <ruin950521@naver.com>
2022-11-18 18:38:53 +0900

    ADD: Request validation-> ObjectId, Request Body

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -1,6 +1,23 @@
 import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl';
+
 const posts = new Router();
+posts.get('/', postsCtrl.list);
+posts.post('/', postsCtrl.write);
+
+const post = new Router(); // /api/posts/:id
+post.get('/', postsCtrl.read);
+post.delete('/', postsCtrl.remove);
+post.patch('/', postsCtrl.update);
+
+posts.use('/:id', postsCtrl.checkObjectId, post.routes());
+
+// posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
+// posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
+// posts.put('/:id', postsCtrl.replace);
+// posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
+
+export default posts;
 
 // const printInfo = (ctx) => {
 //   ctx.body = {
@@ -9,12 +26,3 @@ const posts = new Router();
 //     params: ctx.params,
 //   };
 // };
-
-posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-// posts.put('/:id', postsCtrl.replace);
-posts.patch('/:id', postsCtrl.update);
-
-export default posts;
