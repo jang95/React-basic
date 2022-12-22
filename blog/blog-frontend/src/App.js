0d0b0db06dbf25001bc56154ca5848cc00e8a6f7
jang95 <ruin950521@naver.com>
2022-12-22 20:31:47 +0900

    FIX: router-v6 & ADD: Button

--- blog/blog-frontend/src/App.js
+++ blog/blog-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import PostListPage from './pages/PostListPage';
 import Loginpage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
@@ -9,13 +9,15 @@ import PostPage from './pages/PostPage';
 // PostListPage의 path에 배열을 넣어주면 한 라우트에서 컴포넌트에 여러 개의 경로를 쉽게 설정할 수 있음
 const App = () => {
   return (
-    <>
-      <Route component={PostListPage} path={['@/:username', '/']} exact />
-      <Route component={Loginpage} path="/login" />
-      <Route component={RegisterPage} path="/register" />
-      <Route component={WritePage} path="write" />
-      <Route component={PostPage} path="/@:username/:postId" />
-    </>
+    <Routes>
+      {/* <Route element={PostListPage} path={['@/:username', '/']} /> */}
+      <Route element={<PostListPage />} path="/@:username" />
+      <Route element={<PostListPage />} path="/" />
+      <Route element={<Loginpage />} path="/login" />
+      <Route element={<RegisterPage />} path="/register" />
+      <Route element={<WritePage />} path="write" />
+      <Route element={<PostPage />} path="/@:username/:postId" />
+    </Routes>
   );
 };
 
