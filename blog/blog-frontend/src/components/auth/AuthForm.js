dfe30acc683bcde1ccbf818029d7d9296ee95745
jang95 <81616561+jang95@users.noreply.github.com>
2022-12-24 13:26:42 +0900

    ADD: auth component
    Basic environment configuration and rendering
    
    use snipeet

---
+++ blog/blog-frontend/src/components/auth/AuthForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+/**
+ * 회원가입 또는 로그인 폼을 보여주는 component
+ */
+
+/**
+ * 이번 프로젝트에서는 각 최상위 component를 선언할 때 이름 뒤에 Block이라는 단어를 붙임
+ */
+
+const AuthFromBlock = styled.div``;
+
+const AuthForm = () => {
+  return <AuthFromBlock>AuthForm</AuthFromBlock>;
+};
+
+export default AuthForm;
