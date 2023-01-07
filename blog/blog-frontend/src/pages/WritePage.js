c620e5aa037a240bdc67cb293305129ac4da3ff3
jang95 <81616561+jang95@users.noreply.github.com>
2023-01-07 13:06:54 +0900

    ADD: 에디터 하단 컴포넌트 UI 구현
    useMemo 및 useCallback에 대한 복습 필요

--- blog/blog-frontend/src/pages/WritePage.js
+++ blog/blog-frontend/src/pages/WritePage.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Responsive from '../components/common/Responsive';
-import EditorContainer from '../containers/write/EditorContainer';
-import TagBoxContainer from '../containers/write/TagBoxContainer';
-import WriteActionButtonsContainer from '../containers/write/WriteActionButtonsContainer';
+import Editor from '../components/write/Editor';
+import TagBox from '../components/write/TagBox';
+import WriteActionButtons from '../components/write/WriteActionButtons';
 
 const WritePage = () => {
   return (
     <Responsive>
-      <EditorContainer />
-      <TagBoxContainer />
-      <WriteActionButtonsContainer />
+      <Editor />
+      <TagBox />
+      <WriteActionButtons />
     </Responsive>
   );
 };
