d8067c3bda32d02e7866684169fd6ec790d24be7
jang95 <81616561+jang95@users.noreply.github.com>
2023-01-10 21:34:13 +0900

    ADD: 글쓰기 API 연동

---
+++ blog/blog-frontend/src/lib/api/posts.js
@@ -0,0 +1,10 @@
+import client from './client';
+
+// post관련된 API를 요청하는 함수
+
+export const writePost = ({ title, body, tags }) =>
+  client.post('/api/posts', {
+    title,
+    body,
+    tags,
+  });
