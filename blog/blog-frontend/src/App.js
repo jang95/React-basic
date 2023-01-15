4837027f29c0051498372373768f56144ef53c02
jang95 <81616561+jang95@users.noreply.github.com>
2023-01-15 22:48:21 +0900

    FIX: App 라우터 경로
    @를 붙인 것까지 전부다 :username으로 인식하는거 같음

--- blog/blog-frontend/src/App.js
+++ blog/blog-frontend/src/App.js
@@ -8,26 +8,13 @@ import PostPage from './pages/PostPage';
 
 // PostListPage의 path에 배열을 넣어주면 한 라우트에서 컴포넌트에 여러 개의 경로를 쉽게 설정할 수 있음
 const App = () => {
-  // return (
-  //   <Routes>
-  //     {/* <Route element={PostListPage} path={['@/:username', '/']} /> */}
-  //     <Route element={<PostListPage />} path="/@:username" />
-  //     <Route element={<PostListPage />} path="/" />
-  //     <Route element={<LoginPage />} path="/login" />
-  //     <Route element={<RegisterPage />} path="/register" />
-  //     {/* <Route element={<WritePage />} path="/write" /> */}
-  //     <Route path="/write" element={<WritePage />} />
-  //     {/* <Route element={<PostPage />} path="/@:username/:postId" /> */}
-  //     <Route element={<PostPage />} path="/@:username/:postId" />
-  //   </Routes>
-  // );
   return (
     <Routes>
       <Route path="/" element={<PostListPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/write" element={<WritePage />} />
-      <Route path="/@:username">
+      <Route path="/:username">
         <Route index element={<PostListPage />} />
         <Route path=":postId" element={<PostPage />} />
       </Route>
