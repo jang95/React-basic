6c03cd925c132f9fa60fff82c5ea5f87ab8a207c
jang95 <81616561+jang95@users.noreply.github.com>
2023-01-19 12:47:13 +0900

    포스트 목록 조회 API 연동하기
    ADD: qs

--- blog/blog-frontend/src/lib/api/posts.js
+++ blog/blog-frontend/src/lib/api/posts.js
@@ -1,12 +1,22 @@
 import client from './client';
+import qs from 'qs';
 
 export const writePost = ({ title, body, tags }) =>
   client.post('/api/posts', { title, body, tags });
 
 export const readPost = (id) => client.get(`/api/posts/${id}`);
 
+// export const listPosts = ({ page, username, tag }) => {
+//   return client.get(`/api/posts`, {
+//     params: { page, username, tag },
+//   });
+// };
+
 export const listPosts = ({ page, username, tag }) => {
-  return client.get(`/api/posts`, {
-    params: { page, username, tag },
+  const queryString = qs.stringify({
+    page,
+    username,
+    tag,
   });
+  return client.get(`/api/posts?${queryString}`);
 };
--- blog/blog-frontend/src/pages/PostListPage.js
+++ blog/blog-frontend/src/pages/PostListPage.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
-// import PostListContainer from '../containers/posts/PostListContainer';
-import PostList from '../components/posts/PostList';
+import PostListContainer from '../containers/posts/PostListContainer';
 
 const PostListPage = () => {
   return (
     <>
       <HeaderContainer />
-      {/* <PostListContainer /> */}
-      <PostList />
+      <PostListContainer />
     </>
   );
 };
