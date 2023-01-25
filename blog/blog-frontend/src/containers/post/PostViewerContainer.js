149916513a7125eecaecd6387abf1a832443972d
jang95 <81616561+jang95@users.noreply.github.com>
2023-01-25 15:21:01 +0900

    ADD: 포스트 수정
    1. 수정 버튼 component 제작 -> JSX 형태 사용
    2. 수정 버튼 클릭 시 글쓰기 페이지로 이동
       1) 작성자 일 때만 수정 버튼 나오게 설정
       2) useRef 사용 mount 상황에 따라 작업 처리
    3. 수정 API 사용

--- blog/blog-frontend/src/containers/post/PostViewerContainer.js
+++ blog/blog-frontend/src/containers/post/PostViewerContainer.js
@@ -1,13 +1,25 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { readPost, unloadPost } from '../../modules/post';
 import PostViewer from '../../components/post/PostViewer';
+import PostActionButtons from '../../components/post/PostActionButton';
+import { setOriginalPost } from '../../modules/write';
 
 const PostViewerContainer = () => {
   // 처음 마운트될 때 포스트 읽기 API 요청
   const { postId } = useParams();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const { post, error, loading, user } = useSelector(
+    ({ post, loading, user }) => ({
+      post: post.post,
+      error: post.error,
+      loading: loading['post/READ_POST'],
+      user: user.user,
+    }),
+  );
 
   useEffect(() => {
     dispatch(readPost(postId));
@@ -17,13 +29,21 @@ const PostViewerContainer = () => {
     };
   }, [dispatch, postId]);
 
-  const { post, error, loading } = useSelector(({ post, loading }) => ({
-    post: post.post,
-    error: post.error,
-    loading: loading['post/READ_POST'],
-  }));
+  const onEdit = () => {
+    dispatch(setOriginalPost(post));
+    navigate('/write');
+  };
 
-  return <PostViewer post={post} loading={loading} error={error} />;
+  const ownPost = (user && user._id) === (post && post.user._id);
+
+  return (
+    <PostViewer
+      post={post}
+      loading={loading}
+      error={error}
+      actionButtons={ownPost && <PostActionButtons onEdit={onEdit} />}
+    />
+  );
 };
 
 export default PostViewerContainer;
