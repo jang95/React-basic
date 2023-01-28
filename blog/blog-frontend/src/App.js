262ba1375f745af788809ea3629aaf24f9e733e2
jang95 <81616561+jang95@users.noreply.github.com>
2023-01-28 16:53:59 +0900

    ADD: react-helmet-async
    가장 최상위 컴포넌트를 HelmetProvider로 감싼 뒤 사용

--- blog/blog-frontend/src/App.js
+++ blog/blog-frontend/src/App.js
@@ -5,20 +5,26 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import WritePage from './pages/WritePage';
 import PostPage from './pages/PostPage';
+import { Helmet } from 'react-helmet-async';
 
 // PostListPage의 path에 배열을 넣어주면 한 라우트에서 컴포넌트에 여러 개의 경로를 쉽게 설정할 수 있음
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<PostListPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/write" element={<WritePage />} />
-      <Route path="/:username">
-        <Route index element={<PostListPage />} />
-        <Route path=":postId" element={<PostPage />} />
-      </Route>
-    </Routes>
+    <>
+      <Helmet>
+        <title>REACTER</title>
+      </Helmet>
+      <Routes>
+        <Route path="/" element={<PostListPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/write" element={<WritePage />} />
+        <Route path="/:username">
+          <Route index element={<PostListPage />} />
+          <Route path=":postId" element={<PostPage />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
